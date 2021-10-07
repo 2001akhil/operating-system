54b44dbc00f7831d1ac8c5208f6fcdd3dac11248
Akhil.A <56401501+2001akhil@users.noreply.github.com>
2021-10-07 11:22:04 +0530

    Update FCFS.C

--- FCFS.C
+++ FCFS.C
@@ -1,24 +1,19 @@
 #include<stdio.h>
-struct pcb{
-    int pid,turnarround_time,burst_time,arrival_time;
-
-};
+struct pcb{int pid,arrival_time,turnarround_time,burst_time;};
 void pline(int x);
 void main()
 {
-    struct pcb p[10],temp;
     int num;
     float sum=0.0,avg=0.0;
-
-    printf("Enter the process:");
-    scanf("%d",num);
+    struct pcb p[10],temp;
+    printf("\nEnter the total no. of process:");
+    scanf("%d",&num);
     for(int i=0;i<num;i++)
     {
-        printf("\nEnter the burst time and arrival time:%d",i+1);
+        printf("\nEnter the arrival time and burst time %d: ",i+1);
         scanf("%d%d",&p[i].arrival_time,&p[i].burst_time);
-      p[i].pid=i+1;
-
-
+        p[i].pid=i+1;
+        
     }
     //sort according to the arrival time
     for(int i=0;i<num-1;i++)
@@ -31,38 +26,36 @@ void main()
             p[j]=p[j+1];
             p[j+1]=temp;
             }
-
         }
     }
-    //calculate the turn arround time
+    //calculate turn arround turnarround_time
     for(int i=0;i<num;i++)
     {
         sum=sum+p[i].burst_time;
         p[i].turnarround_time=sum;
-
-
+        
     }
     sum=0;
     pline(44);
-    printf("PID\tArrival time\tBurst time\tTurn arround time\n");
+    printf("PID\tArrival time\t burst_time\turnarround_time\n:");
     pline(44);
-    //each process value
     for(int i=0;i<num;i++)
     {
-        printf("%d\t%d\t%d\t%d\t/n",p[i].pid,p[i].arrival_time,p[i].burst_time,p[i].turnarround_time);
+        printf("%d\t%d\t%d\t%d\n",p[i].pid,p[i].arrival_time,p[i].burst_time,p[i].turnarround_time);
         sum+=p[i].turnarround_time;
-
+        
     }
     pline(44);
     avg=sum/(float)num;
-printf("\nTotal turn arround time %f",sum);
-printf("\nAvg time %f",avg);
-
-
-
-}
-void pline(int x)
-{
-    for(int i=0;i<x;i++)
-    {printf("\n");}printf("\n");
-}
+    printf("\nTOTAL NO. OF turnarround_time:%f",sum);
+    printf("\nAVG turnarround_time:%f",avg);
+    }
+    
+    void pline(int x)
+    {
+        for(int i=0;i<x;i++)
+        {
+        printf("-");
+        } printf("\n");
+    }
+   
